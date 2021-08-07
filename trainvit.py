a30b5177fdbac8b60cfa33df1ecac20ef1299cf3
hzane <hearts.zhang@gmail.com>
2021-08-07 14:42:17 +0800

    - SUPPORTED hydra

--- trainvit.py
+++ trainvit.py
@@ -11,24 +11,56 @@ from typing import Optional
 
 import torch
 import torch.nn as nn
+import pytorch_lightning as pl
+import hydra
 
 from torch.optim import SGD, lr_scheduler, Adam
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
-import pytorch_lightning as pl
+from hydra.utils import to_absolute_path as hydra_to_absolute_path
+
 from vit_pytorch.efficient import ViT
 from linformer import Linformer
 
-# from fire import Fire
 from pathlib import Path
 # %%
 
 
+@dataclass
+class TrainerConf:
+    pass
+
+
+@dataclass
+class ModelConf:
+    pass
+
+
+@dataclass
+class DataConf:
+    pass
+
+
+@dataclass
+class XArtsConfig:
+    trainer: TrainerConf = TrainerConf()
+    model: ModelConf = ModelConf()
+    data: DataConf = DataConf()
+    seed: int = 210820
+    scheme: str = 'xart3'
+    epochs: int = 10
+    num_workers: int = 4
+    batch_size: int = 128
+    lr: float = 3e-5
+    force_save: bool = False
+    num_classes: int = 9
+
+
 class XArtsModule(pl.LightningModule):
-    def __init__(self, **hparams):
+    def __init__(self, conf: XArtsConfig):
         super().__init__()
-        self.save_hyperparameters()
+        self.save_hyperparameters(conf)
         self.criterion = nn.CrossEntropyLoss()
         self.model = self.configure_model()
 
@@ -48,7 +80,7 @@ class XArtsModule(pl.LightningModule):
         optimizer = Adam(self.parameters(), lr = self.hparams.lr)
         return optimizer
 
-    def configure_model(self,):
+    def configure_model(self, ):
         efficient_transformer = Linformer(
             dim = 256,
             seq_len = 49 + 1,
@@ -74,7 +106,7 @@ class XArtsModule(pl.LightningModule):
 class XArtsDataModule(pl.LightningDataModule):
     def __init__(self, data_dir: str, batch_size: int = 16, num_workers = 0):
         super().__init__()
-        self.data_dir = data_dir
+        self.data_dir = hydra_to_absolute_path(data_dir)
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.data = None
@@ -93,47 +125,32 @@ class XArtsDataModule(pl.LightningDataModule):
             self.data,
             shuffle = True,
             batch_size = self.batch_size,
-            pin_memory = True,
+            pin_memory = False,
             num_workers = self.num_workers,
         )
         return dataloader
-
-    def val_dataloader_no(self):
-        pass
-
-    def test_dataloader_no(self):
-        pass
 # %%
 
 
-@dataclass
-class Experiment:
-    scheme: str
-    epochs: int = 10
-    num_workers: int = 4
-    batch_size: int = 128
-    lr: float = 3e-5
-    force_save: bool = False
-    num_classes: int = 9
+@hydra.main(config_path = None, config_name = 'config')
+def xarts_cli(conf: XArtsConfig) -> None:
+    pl.seed_everything(conf.seed)
+    trainer = pl.Trainer(gpus = 1)
+    module = XArtsModule(conf)
+    data = XArtsDataModule(
+        conf.scheme + '.train',
+        conf.batch_size,
+        conf.num_workers,
+    )
 
-    def as_dict(self):
-        return asdict(self)
-
-    def train(self):
-        module = XArtsModule(**self.as_dict())
-        trainer = pl.Trainer(gpus = 1)
-        xarts = XArtsDataModule(self.scheme + '.train', self.batch_size,
-                                self.num_workers, )
-
-        trainer.fit(module, xarts)
-
-# %%
+    trainer.fit(module, data)
 
 
 if __name__ == '__main__':
     '''
     python train.py --scheme=jigsaw
     '''
-    from fire import Fire
+    cs = hydra.core.config_store.ConfigStore.instance()
+    cs.store(name = 'config', node = XArtsConfig)
 
-    Fire(Experiment)
+    xarts_cli()
